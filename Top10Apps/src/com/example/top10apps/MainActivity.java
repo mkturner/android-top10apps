774ff6ae1a2a889abe3aefd998d1b6d1210c4095
Marvin T <contact@marvinkturner.com>
2015-01-06 23:59:40 -0500

    begins implementation of network comm

--- Top10Apps/src/com/example/top10apps/MainActivity.java
+++ Top10Apps/src/com/example/top10apps/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.top10apps;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,5 +38,68 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class DownloadData extends AsyncTask<String, Void, String> {
+		
+		String myXmlData;
+		
+		protected String doInBackground(String... urls) {
+			try{
+				myXmlData = downloadXML(urls[0]);
+				
+			} catch(IOException e){
+				return "Unable to Download XML file.";
+			}
+			
+			return "";
+		}
+		
+		protected void onPostExecute(String result) {
+			Log.d("OnPostExecute", myXmlData); //log
+			
+		}
+		
+		private String downloadXML(String theUrl) throws IOException {
+			int BUFFER_SIZE=2000;
+			InputStream is = null;
+			
+			String xmlContents = "";
+			
+			try{
+				URL url = new URL(theURL);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setReadTimeout(10000);
+				conn.setConnectTimeout(15000);
+				conn.setRequestMethod("GET");
+				conn.setDoInput(true);
+				int response = conn.getResponseCode();
+				Log.d("DownloadXML", "The Response returned is: "+response);
+				is = conn.getInputStream();
+				
+				InputStreamReader isr = new InputStreamReader(is);
+				int charRead;
+				char[] inputBuffer = new char [BUFFER_SIZE];
+				try{
+					while((charRead = isr.read(inputBuffer)) > 0) {
+						String readString = String.copyValueOf(inputBuffer, 0, charRead);
+						xmlContents += readString;
+						inputBuffer = new char[BUFFER_SIZE];
+					}
+					
+					return xmlContents;
+					
+				}catch(IOException e)  {
+					e.printStackTrace();
+					return null;
+				}
+				
+				
+			} finally {
+				if(is != null)
+					is.close();
+			}
+		}
+		
 	}
 }
